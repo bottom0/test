7d62abb768772dbdd730db7c32cb0c78152781a4
Wang Pengyu <80024076+bottom0@users.noreply.github.com>
2023-07-27 15:05:02 +0800

    Create array.py

---
+++ array.py
@@ -0,0 +1,19 @@
+# 用于python数组练习
+
+def array_test():
+    array_list = [1, 2, 3, 4, 5, 6]
+
+    # 使用size()函数读取数组元素个数大小
+    number_array = size(array_list)
+    print(f"array_list数组元素个数为{array_list}")
+
+    # 使用capacity查询数组可容纳元素个数
+    capacity
+
+
+def main():
+    array_test()
+
+
+if __name__ == "__main__":
+    main()
